ac4508107d2571a5c08065a64326ade3b2af45d0
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2025-07-12 11:42:42 -0400

    Update extension

--- frame/extensions/kiiin_frame/kiiin_frame.sh
+++ frame/extensions/kiiin_frame/kiiin_frame.sh
@@ -7,6 +7,9 @@ LOG="$DIR/log.txt"
 echo "Launching at $(date)" >"$LOG"
 echo "DIR=$DIR" >>"$LOG"
 
+echo "Stopping Kindle framework" >"$LOG"
+/etc/init.d/framework stop
+
 # shellcheck disable=SC1090
 if [ -f "$ENV_FILE" ]; then
     echo "Sourcing $ENV_FILE" >>"$LOG"
